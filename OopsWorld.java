1c229d5cc6d47840b241ae344c64e743712d5cac
Pushkar anand <puhkaranand@gmail.com>
2026-02-23 01:44:09 +0530

    feat: add uc1

---
+++ OopsWorld.java
@@ -0,0 +1,12 @@
+class OopsWord {
+    public static void main(String[] args) {
+        // Each letter (O, O, P, S) is 9 characters wide, aligned properly
+        System.out.println("  *****   " + "  *****   " + " ******** " + " ******** ");
+        System.out.println(" *     *  " + " *     *  " + " *       *" + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " *       *" + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " ******** " + " ******** ");
+        System.out.println(" *     *  " + " *     *  " + " *         " + "       *  ");
+        System.out.println(" *     *  " + " *     *  " + " *         " + "       *  ");
+        System.out.println("  *****   " + "  *****   " + " *         " + " ******** ");
+    }
+}
