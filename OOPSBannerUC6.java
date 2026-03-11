77ef51e7883c30f2552e55bf304041a645ade79a
Pushkar anand <puhkaranand@gmail.com>
2026-03-11 22:20:31 +0530

    feat: add uc6

---
+++ OOPSBannerUC6.java
@@ -0,0 +1,54 @@
+public class OOPSBannerUC6 {
+
+    public static String[] getOpattern() {
+        return new String[]{
+            "   ***   ",
+            " **   ** ",
+            "**     **",
+            "**     **",
+            "**     **",
+            " **   ** ",
+            "   ***   "
+        };
+    }
+
+    public static String[] getPpattern() {
+        return new String[]{
+            "******** ",
+            "*       *",
+            "*       *",
+            "******** ",
+            "*        ",
+            "*        ",
+            "*        "
+        };
+    }
+
+    public static String[] getSpattern() {
+        return new String[]{
+            " ******* ",
+            "*        ",
+            "*        ",
+            " ******* ",
+            "        *",
+            "        *",
+            " ******* "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] Opattern = getOpattern();
+        String[] Ppattern = getPpattern();
+        String[] Spattern = getSpattern();
+
+        for (int i = 0; i < Opattern.length; i++) {
+            System.out.println(
+                Opattern[i] + "  " +
+                Opattern[i] + "  " +
+                Ppattern[i] + "  " +
+                Spattern[i]
+            );
+        }
+    }
+}
